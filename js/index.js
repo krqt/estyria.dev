5ebcbfe4d8e1acb9979665400ea40c786f973f37
krqt <32986257+krqt@users.noreply.github.com>
2025-06-25 17:53:26 -0400

    Add vTubie

---
+++ js/index.js
@@ -0,0 +1,14 @@
+console.log("JS loading...");
+
+const site = "https://estyria.dev/";
+
+const links = [
+    {slug: "yt", name: "YouTube"},
+    {slug: "ttv", name: "Twitch"},
+    {slug: "bsk", name: "Bluesky"}
+];
+
+window.addEventListener('load',
+    (event) => {
+        console.log("Page loaded");    
+    }, false);
